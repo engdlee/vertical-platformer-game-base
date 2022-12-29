416cac37fd2f651f07765af5f81f458b9b633f3f
David Lee <ing.jdplee@gmail.com>
2022-12-29 17:57:05 -0400

    Gravity Step

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,58 @@ if (canvas) {
   canvas.height = 576;
 }
 
-if (c && canvas) {
-  c.fillStyle = "white";
-  c.fillRect(0, 0, canvas.width, canvas.height);
+const gravity = 0.5;
+interface Position {
+  x: number;
+  y: number;
 }
+interface Velocity extends Position {}
+class Player {
+  position: Position;
+  velocity: Velocity;
+  height: number;
+  constructor(position: Position) {
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
+  }
+
+  draw() {
+    if (c) {
+      c.fillStyle = "red";
+      c.fillRect(this.position.x, this.position.y, 100, this.height);
+    }
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    if (
+      canvas &&
+      this.position.y + this.height + this.velocity.y < canvas.height
+    ) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player = new Player({ x: 0, y: 0 });
+const player2 = new Player({ x: 0, y: 0 });
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  if (c && canvas) {
+    c.fillStyle = "white";
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    player.draw();
+    player.update();
+  }
+}
+
+animate();
