30164f7b2e6502e9df6b1b97e4d7885a963574ea
David Lee <ing.jdplee@gmail.com>
2022-12-29 18:51:20 -0400

    Background sprite Step

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import "./style.css";
 const canvas = document.querySelector<HTMLCanvasElement>("#app");
 const c = canvas?.getContext("2d");
 
+let scaledCanvas = { with: 0, height: 0 };
 if (canvas) {
   canvas.width = 1024;
   canvas.height = 576;
+
+  scaledCanvas.with = canvas.width / 4;
+  scaledCanvas.height = canvas.height / 4;
 }
 
 const gravity = 0.5;
@@ -14,6 +18,34 @@ interface Position {
   y: number;
 }
 interface Velocity extends Position {}
+
+interface ISprite {
+  position: Position;
+  imageSrc: string;
+}
+class Sprite {
+  position: Position;
+  image: HTMLImageElement;
+
+  constructor({ position, imageSrc }: ISprite) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw() {
+    if (!this.image) {
+      return;
+    }
+    if (c) {
+      c.drawImage(this.image, this.position.x, this.position.y);
+    }
+  }
+
+  update() {
+    this.draw();
+  }
+}
 class Player {
   position: Position;
   velocity: Velocity;
@@ -61,14 +93,29 @@ const keys = {
   },
 };
 
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/background.png",
+});
+
 function animate() {
   window.requestAnimationFrame(animate);
   if (c && canvas) {
     c.fillStyle = "white";
     c.fillRect(0, 0, canvas.width, canvas.height);
 
+    c.save();
+    c.scale(4, 4);
+    c.translate(0, -background.image.height + scaledCanvas.height);
+    background.update();
+    c.restore();
+
     player.update();
     player.velocity.x = 0;
+
     if (keys.d.pressed) {
       player.velocity.x = 5;
     } else if (keys.a.pressed) {
